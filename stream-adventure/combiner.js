654b5a1152de8474ee97fdfa9986741bc4382a65
Abdullah Alansari <ahimta@gmail.com>
2015-01-09 08:56:16 +0300

    finish: one exercise in 'stream adventure' workshopper

--- stream-adventure/combiner.js
+++ stream-adventure/combiner.js
@@ -1,16 +1,38 @@
-const through2map = require('through2-map'),
-      combine     = require('stream-combiner'),
-      split       = require('split')
+const through = require('through')
+      combine = require('stream-combiner'),
+      split   = require('split'),
+      zlib    = require('zlib'),
+      os      = require('os')
 
 module.exports = function () {
-  h = {}
 
-  combine(
-    process.openStdin(),
-    split(),
-    through2map({wantStrings: true}, function (line) {
-      console.log(line)
-      return line
-    })
-  )
+  const grouper = through(write, end)
+
+  return combine(split(), grouper, zlib.createGzip())
+
+  var currentGenre = null
+
+  function write (line) {
+    if (line.length === 0) { return }
+
+    const json = JSON.parse(line)
+    const currentType = json['type']
+
+    if (currentType === 'genre') {
+      if (currentGenre) {
+        this.queue(JSON.stringify(currentGenre) + os.EOL)
+      }
+      currentGenre = {name: json['name'], books: []}
+    }
+    else if (currentType === 'book') {
+      currentGenre.books.push(json['name'])
+    }
+  }
+
+  function end () {
+    if (currentGenre) {
+      this.queue(JSON.stringify(currentGenre) + os.EOL)
+    }
+    this.queue(null)
+  }
 }
